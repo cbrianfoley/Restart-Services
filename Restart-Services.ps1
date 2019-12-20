df7c369227f3f689035b4eb6bf41cfa0b7ff5aad
cbrianfoley <cbrianfoley@users.noreply.github.com>
2019-12-20 10:45:52 -0600

    Initial commit

---
+++ Restart-Services.ps1
@@ -0,0 +1,39 @@
+﻿$Services = "VMMS","VSS"
+
+foreach ($service in $Services){
+    $servicestate = Get-Service $service -ErrorAction SilentlyContinue
+    if ($Error){
+        "$($service) service doesn't exist, or another error occured."
+        $Error.Clear()
+    }
+    if ($servicestate.Status -eq 'Stopped'){
+        try{
+            "$($service) service is stopped. Starting."
+            Start-Service $service -Verbose
+        }
+        catch { 
+            "Error starting service. Attempting to use force"
+        }
+    }
+    if ($servicestate.Status -eq 'Running'){
+        "$($service) service is Running. Restarting."
+        try{
+            Stop-Service $service -Verbose
+        }
+        catch{
+            "Error stopping. Attempting to use force."
+            Stop-Service $service -Force -Verbose
+        }
+        try{
+            Start-Service $service -Verbose
+        }
+        catch{
+            "Error starting service."
+        }
+    }
+    else {
+        "$($service) is $($servicestate). Attempting to restart using force."
+        Stop-Service $service -Force -Verbose
+        Start-Service $service
+    }
+}
